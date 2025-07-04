70ce6492726466a27fc7e66d7ab0b3c2daccdfa6
Marco Bacci <marcobacci94@gmail.com>
2025-07-04 15:57:58 +0200

    feat: declare const

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
+// -------------MAIL---------------
+// // dichiaro array di lista di mails e const prompt mail cliente
+// const mail = [`marco@gmail.com` , `lily@icloud.com`,`chiara@hotmail.it` , `alex@gmail.com` , `jesus@paradise.com` , `Thomas@icloud.com` ]
+// const userMail = (prompt(`scrivi la tua email`))
 
-// dichiaro array di lista di mails e const prompt mail cliente
-const mail = [`marco@gmail.com` , `lily@icloud.com`,`chiara@hotmail.it` , `alex@gmail.com` , `jesus@paradise.com` , `Thomas@icloud.com` ]
-const userMail = (prompt(`scrivi la tua email`))
-let message = `la tua mail è nella lista e puoi accedere alla festa!`
+// //  inizializzo ciclo for dove userMail deve essere contenuto in Mail
+// for(let i=0; i<mail.length; i++){
+//   if (userMail === mail[i]){
+//     console.log(`la tua mail è nella lista e puoi accedere alla festa!`);
+//   }
+//   else
+//   console.log(`Non sei in lista`)
+// }
 
-for(let i=0; i<mail.length; i++){
-  if (userMail === mail[i])
-    console.log(message)
-  else
-  console.log(`Non sei in lista`)
-}
-  
+// ------------DADI-------------
+
+// dichiaro const numero random per pc e per giocatore
+const pcNum = Math.floor(Math.random(6)+1)
+const playerNum = prompt(`scrivi un numero da 1 a 6`)
+
+
+
+
+
+
+    
 
     
     
