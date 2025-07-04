a0a030b96a70eeffdc23b36320b42b4edfb96918
Marco Bacci <marcobacci94@gmail.com>
2025-07-04 16:43:34 +0200

    fix: declare let message

--- js/script.js
+++ js/script.js
@@ -1,34 +1,35 @@
 // -------------MAIL---------------
 // // dichiaro array di lista di mails e const prompt mail cliente
-// const mail = [`marco@gmail.com` , `lily@icloud.com`,`chiara@hotmail.it` , `alex@gmail.com` , `jesus@paradise.com` , `Thomas@icloud.com` ]
-// const userMail = (prompt(`scrivi la tua email`))
+const mail = [`marco@gmail.com` , `lily@icloud.com`,`chiara@hotmail.it` , `alex@gmail.com` , `jesus@paradise.com` , `Thomas@icloud.com` ]
+const userMail = (prompt(`scrivi la tua email`))
+let message
 
-// //  inizializzo ciclo for dove userMail deve essere contenuto in Mail
-// for(let i=0; i<mail.length; i++){
-//   if (userMail === mail[i]){
-//     console.log(`la tua mail è nella lista e puoi accedere alla festa!`);
-//   }
-//   else
-//   console.log(`Non sei in lista`)
-// }
+//  inizializzo ciclo for dove userMail deve essere contenuto in Mail
+for(let i=0; i<mail.length; i++){
+  if (userMail === mail[i]){
+    message = `la tua mail è nella lista e puoi accedere alla festa!`;
+  }
+  else
+    message = `Non sei in lista`
+}
 
 // ------------DADI-------------
 
-// dichiaro const numero random per pc e per giocatore
-const pcNum = Math.floor((Math.random()*6 +1))
-const playerNum = prompt(`scrivi un numero da 1 a 6`)
-let result
-// comparo il numero del pc con quello del player
-if( playerNum > pcNum){
-  result = `${playerNum} è maggiore di ${pcNum} e quindi hai VINTO!`
-}
-else if(playerNum < pcNum){
-  result = `${playerNum} è minore di ${pcNum} e quindi hai PERSO`
-}
-else{
-  result = `${playerNum} è uguale a ${pcNum} e quindi hai PAREGGIATO`
-}
-console.log(result)
+// // dichiaro const numero random per pc e per giocatore
+// const pcNum = Math.floor((Math.random()*6 +1))
+// const playerNum = prompt(`scrivi un numero da 1 a 6`)
+// let result
+// // comparo il numero del pc con quello del player
+// if( playerNum > pcNum){
+//   result = `${playerNum} è maggiore di ${pcNum} e quindi hai VINTO!`
+// }
+// else if(playerNum < pcNum){
+//   result = `${playerNum} è minore di ${pcNum} e quindi hai PERSO`
+// }
+// else{
+//   result = `${playerNum} è uguale a ${pcNum} e quindi hai PAREGGIATO`
+// }
+// console.log(result)
 
 
 
