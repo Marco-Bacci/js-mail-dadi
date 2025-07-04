f212692fef361dca68166d135fc0a585ca18e084
Marco Bacci <marcobacci94@gmail.com>
2025-07-04 15:12:54 +0200

    feat: declare const

---
+++ js/script.js
@@ -0,0 +1,5 @@
+
+// dichiaro array di lista di mails e const prompt mail cliente
+const mail = [`marco@gmail.com` , `lily@icloud.com`,`chiara@hotmail.it` , `alex@gmail.com` , `jesus@paradise.com` , `Thomas@icloud.com` ]
+const userMail = (prompt(`scrivi la tua email`))
+
