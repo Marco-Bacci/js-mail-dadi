210a5b95be2b2ad07b87b6238858cca1e6ee2aea
Marco Bacci <marcobacci94@gmail.com>
2025-07-04 15:31:35 +0200

    feat: cicle for + if else

--- js/script.js
+++ js/script.js
@@ -2,4 +2,16 @@
 // dichiaro array di lista di mails e const prompt mail cliente
 const mail = [`marco@gmail.com` , `lily@icloud.com`,`chiara@hotmail.it` , `alex@gmail.com` , `jesus@paradise.com` , `Thomas@icloud.com` ]
 const userMail = (prompt(`scrivi la tua email`))
+let message = `la tua mail è nella lista e puoi accedere alla festa!`
 
+for(let i=0; i<mail.length; i++){
+  if (userMail === mail[i])
+    console.log(message)
+  else
+  console.log(`Non sei in lista`)
+}
+  
+
+    
+    
+  
